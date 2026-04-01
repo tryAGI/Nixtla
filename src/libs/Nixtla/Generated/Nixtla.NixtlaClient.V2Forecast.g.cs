b803558afabe16ed5480da76102db2ca239b5b0f
Konstantin S. <havendv@gmail.com>
2026-04-01 09:15:56 +0400

    feat: Updated OpenAPI spec (#6)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Nixtla/Generated/Nixtla.NixtlaClient.V2Forecast.g.cs
+++ src/libs/Nixtla/Generated/Nixtla.NixtlaClient.V2Forecast.g.cs
@@ -275,7 +275,6 @@ namespace Nixtla
             bool? featureContributions = default,
             global::System.Threading.CancellationToken cancellationToken = default)
         {
-
             var __request = new global::Nixtla.ForecastInput
             {
                 Series = series,
